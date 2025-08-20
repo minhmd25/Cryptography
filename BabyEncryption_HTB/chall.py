e9aa88f6c99adf7b114f27ddb559f2316ff913c0
minhmd25 <minhmd25@gmail.com>
2025-08-20 20:05:26 +0700

    one more

---
+++ BabyEncryption_HTB/chall.py
@@ -0,0 +1,17 @@
+import string
+#from secret import MSG
+
+def encryption(msg):
+    ct = []
+    for char in msg:
+        ct.append((123 * ord(char) + 18) % 256)
+    return bytes(ct)
+
+# ct = encryption(MSG)
+# f = open('./msg.enc','w')
+# f.write(ct.hex())
+# f.close()
+
+s = "hello"
+re = encryption(s)
+print(re.hex())
---
+++ BabyEncryption_HTB/decryp.py
@@ -0,0 +1,12 @@
+mg ="6e0a9372ec49a3f6930ed8723f9df6f6720ed8d89dc4937222ec7214d89d1e0e352ce0aa6ec82bf622227bb70e7fb7352249b7d893c493d8539dec8fb7935d490e7f9d22ec89b7a322ec8fd80e7f8921"
+
+def decode(msg):
+    msg_b = bytes.fromhex(msg)
+    res = ""
+    inv = pow(123, -1, 256)
+    for char in msg_b:
+        res += chr((inv * (char - 18)) % 256)
+    return res
+
+kq = decode(mg)
+print(kq) 
